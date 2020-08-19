0105f472e12249957d5781e757583224b8a0006c
zhangyang <zhangyang02@wanxue.cn>
2020-08-19 14:21:47 +0800

    二级菜单嵌套显示

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,43 @@
 <template>
-   <div>
-     Home 组件
-     <hr>
-     退出登陆
-     <hr>
-     <el-button type="info" @click="logout">退出登陆</el-button>
-   </div>
+  <el-container>
+    <!-- 头部区域 -->
+    <!--el-header 组件名称就是类名 -->
+    <el-header>
+    <div>
+      <img src="../assets/logo.png" alt="logo">
+      电商后台管理系统
+    </div>
+      <el-button type="info" @click="logout">退出登陆</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜单 -->
+          <el-submenu index="1">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-location"></i>
+              <!-- 文本 -->
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 页面内容区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -21,5 +53,50 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373f41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      height: 40px;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
 
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont{
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  // 鼠标放上去变成小手
+  cursor: pointer;
+}
 </style>
