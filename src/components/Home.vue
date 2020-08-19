07372c9928e8fc445a99cd2009f96b06636eb7c9
zhangyang <zhangyang02@wanxue.cn>
2020-08-19 12:57:27 +0800

    路由导航守卫

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,23 @@
 <template>
    <div>
      Home 组件
+     <hr>
+     退出登陆
+     <hr>
+     <el-button type="info" @click="logout">退出登陆</el-button>
    </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    logout () {
+      // 清空回话信息
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
