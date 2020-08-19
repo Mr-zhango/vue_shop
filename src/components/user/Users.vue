4147c4e1ccee32629bef59907de26d304d14e2b1
zhangyang <zhangyang02@wanxue.cn>
2020-08-19 18:02:12 +0800

    表格组件的使用

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,12 +1,65 @@
 <template>
   <div>
-    <h3>用户列表组件</h3>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域  :span="7" 一共24个注意-->
+    <el-card>
+      <!-- 搜索与添加区域 -->
+      <el-row :gutter="20">
+        <el-col :span="7">
+          <el-input placeholder="请输入内容" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!-- 用户列表区域 -->
+      <el-table :data="userList" style="width: 100%" :border="true" :stripe="true">
+        <el-table-column prop="username" label="姓名" width="180"/>
+        <el-table-column prop="email" label="邮箱" width="180"/>
+        <el-table-column prop="mobile" label="电话"/>
+        <el-table-column prop="role_name" label="角色"/>
+        <el-table-column prop="mg_state" label="状态"/>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Users'
+  data () {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList () {
+      const { data: res } = await this.$http.get('users', { params: this.queryInfo })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败!')
+      }
+      console.log(res)
+      this.userList = res.data.users
+      this.total = res.data.total
+    }
+  }
 }
 </script>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -12,6 +12,13 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
 
 // 把弹框组件 进行全局挂载
 Vue.prototype.$message = Message
