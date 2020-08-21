66567d0083ff985e19dc7ad24e05b6797726c4c3
zhangyang <zhangyang02@wanxue.cn>
2020-08-21 10:37:35 +0800

    添加用户完成

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -58,7 +58,7 @@
         </el-pagination>
     </el-card>
     <!-- 添加用户的对话框 -->
-    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
       <!-- 内容主体区域 -->
       <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px" class="demo-ruleForm">
         <el-form-item label="用户名" prop="username">
@@ -77,7 +77,7 @@
       <!-- 底部区域 -->
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -183,6 +183,28 @@ export default {
         return this.$message.error('更新用户状态失败')
       }
       this.$message.success('更新用户状态成功')
+    },
+    // 添加对话框关闭的事件
+    addDialogClosed () {
+      this.$refs.addFormRef.resetFields()
+    },
+    // 点击按钮添加新用户
+    addUser () {
+      this.$refs.addFormRef.validate(async validate => {
+        // console.log(validate)
+        if (!validate) return
+        // 发起添加用户的请求
+        const { data: res } = await this.$http.post('users', this.addForm)
+
+        if (res.meta.status !== 201) {
+          this.$message.error('添加用户失败')
+        }
+        this.$message.success('添加用户成功')
+        // 关闭添加窗口
+        this.addDialogVisible = false
+        // 重新查询用户列表
+        this.getUserList()
+      })
     }
   }
 }
