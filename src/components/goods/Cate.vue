fa61464701bc761c20adf917ce83eb09d218f3b9
zhangyang <zhangyang02@wanxue.cn>
2020-08-24 11:06:06 +0800

    商品分类组件显示

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    商品分类组件
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+
+  },
+  created () {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
